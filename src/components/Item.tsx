bea8e9c7e88fada46048631c5c2d383d17f2c206
ParLelya <parlelya@vk.com>
2023-03-07 17:49:11 +0300

    Update Item.tsx

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -7,13 +7,16 @@ const Item: React.FC<IProductItem> = (props) => {
 
 	const { id, name, images } = props
 	//TODO: компонент не отрисовывается, но ссылка обновляется, надо думать как фиксить
+	
+	const url = images.split(',')[0].replace(/\[|\]|\{|\}|\'/g, '').split(' ')[1]
+	
 	return (
 		<Link
 			className="product-item"
 			to={`/products/${id}`}
 		>
 			{/* TODO: доставать первую картинку из массива */}
-			<img src={images} alt='' />
+			<img src={url} alt='' />
 			<h6 className="amber-text text-darken-3">{name}</h6>
 		</Link>
 	)
