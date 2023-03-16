c320ed79a7dc5c41c8df4037ef3257ffa401bd0d
ParLelya <parlelya@vk.com>
2023-03-16 19:31:56 +0300

    Update Cabinet.tsx

--- src/pages/Cabinet.tsx
+++ src/pages/Cabinet.tsx
@@ -1,44 +1,47 @@
 import React, { useEffect } from 'react'
 import AuthForm from '../components/AuthForm'
-import Loader from '../components/Loader'; 
+import Loader from '../components/Loader';
 import { checkAuth, fetchUserInfo } from './../slices/authSlice';
 import { useAppSelector, useAppDispatch } from '../store/hooks';
 import { RootState } from '../store/store';
 
 const Cabinet: React.FC = () => {
 	const dispatch = useAppDispatch()
-	const {isLoading, isAuth, username, email, id} = useAppSelector((state: RootState) => state.auth)
+	const { isLoading, isAuth, username, email, id } = useAppSelector((state: RootState) => state.auth)
 
 	useEffect(() => {
 		if (localStorage.getItem('token')) {
 			dispatch(checkAuth())
 			dispatch(fetchUserInfo())
 		}
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
+
+
+	// isLoading
+	// ? <Loader />
+	// : 
 
 	return (
 		<div className='cabinet'>
 			{
-				isLoading 
-				? <Loader/>
-				: isAuth
+				isAuth
 					? (
 						<div className='profile'>
-							<h3>Ваш профиль</h3>
-							<div className='profile-info-wrapper'>
-								<label className='profile-label'>Ваш id:</label>
-								<span className='btn profile-info'>{id}</span>
+								<h3>Ваш профиль</h3>
+								<div className='profile-info-wrapper'>
+									<label className='profile-label'>Ваш id:</label>
+									<span className='btn profile-info'>{id}</span>
+								</div>
+								<div className='profile-info-wrapper'>
+									<label className='profile-label'>Имя пользователя:</label>
+									<span className='btn profile-info'>{username}</span>
+								</div>
+								<div className='profile-info-wrapper'>
+									<label className='profile-label'>Ваш емейл:</label>
+									<span className='btn profile-info'>{email}</span>
+								</div>
 							</div>
-							<div className='profile-info-wrapper'>
-								<label className='profile-label'>Имя пользователя:</label>
-								<span className='btn profile-info'>{username}</span>
-							</div>
-							<div className='profile-info-wrapper'>
-								<label className='profile-label'>Ваш емейл:</label>
-								<span className='btn profile-info'>{email}</span>
-							</div>
-						</div>
 					)
 					: <AuthForm />
 			}
