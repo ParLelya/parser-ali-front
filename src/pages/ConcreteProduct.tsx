a86fbc5e5b5ddedf17ef5272a352200d38ea4b68
ParLelya <parlelya@vk.com>
2023-04-04 17:45:10 +0300

    Working on selecting parameters

--- src/pages/ConcreteProduct.tsx
+++ src/pages/ConcreteProduct.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useNavigate, useParams } from 'react-router';
-import { IProductItem } from '../types/interface';
+import { IProductItem, parameters } from '../types/interface';
 import $api from '../http';
 import { useAppSelector } from '../store/hooks';
 import { RootState } from '../store/store';
@@ -28,17 +28,16 @@ const ConcreteProduct: React.FC = () => {
 	}, [isAuth])
 
 	const { id } = useParams()
-	const [item, setItem] = useState<IProductItem>({ 
-		id: 0, name: '', 
-		images: '', 
-		unique_id: '', 
-		from_whom: '', 
-		prices: '', 
-		parameters: '', 
-		additional_parameters: '' 
+	const [item, setItem] = useState<IProductItem>({
+		id: 0, name: '',
+		images: '',
+		unique_id: '',
+		from_whom: '',
+		prices: '',
+		parameters: [],
+		additional_parameters: ''
 	})
-	const [param, setParam] = useState<IParamsObj[]>()
-
+	const [params, setParams] = useState<IParamsObj[]>()
 
 	useEffect(() => {
 		$api.get<IProductItem>(`/api/products/${id}/`)
@@ -49,17 +48,17 @@ const ConcreteProduct: React.FC = () => {
 				const additional_parameters = response.data.additional_parameters
 
 				const prices = response.data.prices //сделать массив и развернуть его
-				
+
 				const images = response.data.images
-				const fixedImages = images.replace(/\'/g, '\"')
+				const fixedImages = images.replace(/'/g, '"')
 				const parsedImages = JSON.parse(fixedImages)
 				const url = parsedImages[0].image
 
-				const parameters = response.data.parameters
-				const fixedParameters = parameters!.replace(/\'/g, '\"')
+				const parameters = response.data.parameters as string
+				const fixedParameters = parameters.replace(/'/g, '"')
 				const parsedParameters = JSON.parse(fixedParameters)
 
-				setParam(parsedParameters)
+				setParams(parsedParameters)
 
 				const item: IProductItem = {
 					id: Number(id),
@@ -77,6 +76,24 @@ const ConcreteProduct: React.FC = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
+	const [selectedParam, setSelectedParam] = useState<parameters[]>([])
+	const inputRef = useRef<HTMLInputElement | null>(null)
+
+	const selectedParams: parameters[] = []
+
+	const selectParams = (title: string, name: string) => {
+		if (inputRef.current?.checked) {
+			selectedParams.push({
+				title: title,
+				info: {
+					name: name
+				}
+			})
+		}
+		console.log(selectedParams)
+		setSelectedParam(selectedParams)
+	}
+
 	return (
 		<div className='product-item'>
 			{/* TODO: реализовать карусель из картинок, вытягивая ссылки по ключу */}
@@ -87,31 +104,36 @@ const ConcreteProduct: React.FC = () => {
 				</div>
 				<div className="card-content">
 					<span className="card-title activator">{item.name}<i className="material-icons right">more_vert</i></span>
-					<div>{
-						param?.map((item, index) => {
-							return (
-								<div className='product-info' key={index}>
-									<h6 style={{ margin: '1rem 0' }}>{item.title}</h6>
-									<div className='parameter'>
-										{
-											item.info.map((detail, id) => (
-												<p key={id} className='parameter-button'>
-													<label className='parameter-label'>
-														<input
-															type="radio"
-															name={item.title}
-														/>
-														<span>{detail.name}</span>
-														{/* <span>{item.prices[id]}</span> */}
-													</label>
-												</p>
-											))
-										}
+					<div>
+						{
+							params?.map((item, index) => {
+								return (
+									<div className='product-info' key={index}>
+										<h6 style={{ margin: '1rem 0' }}>{item.title}</h6>
+										<div className='parameter'>
+											{
+												item.info.map((detail, id) => (
+													<p key={id} className='parameter-button'>
+														<label className='parameter-label'>
+															<input
+																ref={inputRef}
+																id={detail.id}
+																onChange={() => selectParams(item.title, detail.name)}
+																type="radio"
+																name={item.title}
+															/>
+															<span>{detail.name}</span>
+															{/* <span>{item.prices[id]}</span> */}
+														</label>
+													</p>
+												))
+											}
+										</div>
 									</div>
-								</div>
-							)
-						})
-					}</div>
+								)
+							})
+						}
+					</div>
 				</div>
 				<div className="card-reveal">
 					<span className="card-title grey-text text-darken-4">{item.name}<i className="material-icons right">close</i></span>
@@ -121,7 +143,7 @@ const ConcreteProduct: React.FC = () => {
 					/>
 				</div>
 			</div>
-			<Dropdown {...item} />
+			<Dropdown {...item} parameters={selectedParam} />
 		</div >
 	)
 }
